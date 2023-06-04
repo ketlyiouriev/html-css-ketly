f30fb1f6d252438ef03d46df92aab09f388071d7
ketlyiouriev <ketly.iouriev@ut.ee>
2023-06-04 15:10:06 +0300

    phase 2 updates done

--- scripts/about_script.js
+++ scripts/about_script.js
@@ -1,4 +1,3 @@
-/* about.html javascript */
 const accordion_elements = document.getElementsByClassName("accordion-label");
 
 for (let i = 0; i < accordion_elements.length; i++) {
@@ -14,21 +13,3 @@ for (let i = 0; i < accordion_elements.length; i++) {
     }
   });
 }
-
-/* contact.html javascript */
-const login_form = document.getElementsByTagName("form");
-
-login_form[0].addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const first_name = document.getElementById("fname");
-  const email = document.getElementById("email");
-
-  if (first_name.value == "" || email.value == "") {
-    window.alert("Jätsid mõne kohustusliku välja täitmata! Proovi uuesti!");
-    window.location.href = "contact.html";
-  } else {
-    window.alert("Küsimustik edukalt täidetud!");
-    window.location.href = "thankyou.html";
-  }
-});
